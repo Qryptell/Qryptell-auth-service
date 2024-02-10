e96558aa93d1c738cccabdf4c1c6ba85f08d9f41
Vaishakh GK <vaishakhgk2006@gmail.com>
2024-02-10 16:01:59 +0530

    fix: server crash on '/refresh-token'
    Fix server crash when cookies is passed and user with matching
      refreshId/session id not present in databases
    
    Resolves: #14

--- src/auth/refreshToken.js
+++ src/auth/refreshToken.js
@@ -10,10 +10,13 @@ export default function refreshAccessToken(req, res) {
     const getRefreshTokenQuery = `SELECT * FROM auth WHERE session_id="${sessionId}"`
     const deleteUserWithSessionIdQuery = `DELETE FROM auth WHERE session_id="${sessionId}"`
 
-    if (!refreshToken) return res.status(400).json({ success: false, message: "Missing refresh token" })
+    if (!refreshToken) return res.status(401).json({ success: false, message: "Missing refresh token" })
 
-    sql(getRefreshTokenQuery).then(async ([{ refresh_token, user_id }]) => {
-        console.log(user_id);
+    sql(getRefreshTokenQuery).then(async (list) => {
+        if (list.length == 0) {
+            return res.status(401).json({ success: false, message: "Authuntication Failed" })
+        }
+        var { refresh_token, user_id } = list[0]
         const [user] = await sql(`select * from users where _id="${user_id}"`)
         if (refresh_token === refreshToken) {
             const accessToken = jwt.sign({ email: user.email, user_id, username: user.username }, process.env.JWT_ACCESS_SECRET, { expiresIn: '10h', issuer: 'lunarloom_auth:service' })
